74f439c916b287d42b3a43af5e7f95ba12a8325a
Angelo Marques <angelomarquesdeoliveira@gmail.com>
2025-08-05 17:32:15 -0300

    Lixo ideia antiga

--- GestorBibliotecaComunitaria/GestorBiblioteca.Application/InputModels/UpdateEmprestimoInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestorBibliotecaApplication.InputModels
-{
-    public class UpdateEmprestimoInputModel
-    {
-        public int Id { get; set; }
-        public DateTime DataEmprestimo { get; set; }
-        public DateTime DataDevolucao { get; set; }
-    }
-}
